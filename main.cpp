52e670a011bbbf19c80e35516947440c30eea7fb
dengze <493278641@qq.com>
2023-01-04 16:52:53 +0800

    完成hw02

--- main.cpp
+++ main.cpp
@@ -4,32 +4,40 @@
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next;
+    Node*                 prev;
     // 如果能改成 unique_ptr 就更好了!
 
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
+    Node(int val):
+        next(nullptr),
+        prev(nullptr),
+        value(val)
+    {
     }
 
     void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
+        auto node = std::make_unique<Node>(val);
+                                        
+        if(this->next != nullptr){
+            node->next = std::move(this->next);
+            node->prev = this;
+        }
+        this->next = std::move(node);
+        this->next->prev = this->next.get();
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
-        if (next)
-            next->prev = prev;
+        if(next != nullptr)
+        {
+            this->next->prev = this->prev;
+        }            
+        if(prev != nullptr)
+        {
+            this->prev->next = std::move(this->next);
+        }
     }
 
     ~Node() {
@@ -38,13 +46,27 @@ struct Node {
 };
 
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node> head;
 
     List() = default;
 
-    List(List const &other) {
+    List(List const &other)
+    {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        //head = other.head;  // 这是浅拷贝！
+        Node*  other_node = other.head.get();
+
+        this->head = std::make_unique<Node>(other.head->value);
+        Node* cursor_node = this->head.get();                   
+
+        while(other_node->next !=  nullptr){
+            std::unique_ptr<Node> temp_next = std::make_unique<Node>(other_node->next->value);
+            temp_next->prev = cursor_node;
+            cursor_node->next = std::move(temp_next);
+            cursor_node = cursor_node->next.get();
+            other_node = other_node->next.get();
+        }
+
         // 请实现拷贝构造函数为 **深拷贝**
     }
 
@@ -59,16 +81,16 @@ struct List {
 
     int pop_front() {
         int ret = head->value;
-        head = head->next;
+        head = std::move(head->next);
         return ret;
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+        auto node = std::make_unique<Node>(value);
+        node->next = std::move(head);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        head = std::move(node);
     }
 
     Node *at(size_t index) const {
@@ -80,7 +102,7 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List& lst) {  // 有什么值得改进的？
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
